dc3d7c8fc81dbcc8ceeb4ed953e9e21cb48f8df6
aviadr1 <aviadr1@1f8b7f26-7806-0410-ba70-a23862d07478>
2006-05-23 19:32:32 +0000

    bug fixes, discovered with unit-tests

--- trunk/engine/src/SeqWeight.cpp
+++ trunk/engine/src/SeqWeight.cpp
@@ -6,6 +6,7 @@
 #include "persistance/TextReader.h"
 #include "persistance/StdInputStream.h"
 
+#include <boost/algorithm/string.hpp>
 #include <string>
 #include <fstream>
 #include <iostream>
@@ -13,25 +14,25 @@
 
 double SeqWeightDB::PositionalWeight::getPositionWeight (int index) const
 {
-   //
-   // we can use find_if because I don't expect more than
-   // 10 hot-spots in a sequence, and in this range 
-   // find_if performs better than binary_search
-   Positions::const_iterator it = _positions.begin ();
-   for (; it != _positions.end () ; ++it) {
-      if (index < (*it)->_startIndex){
-         it = _positions.end ();
-         break;
-      }
-      else if (index <= (*it)->_startIndex + (*it)->_length)
-         break;
-   }
+	//
+	// we can use find_if because I don't expect more than
+	// 10 hot-spots in a sequence, and in this range 
+	// find_if performs better than binary_search
+	Positions::const_iterator it = _positions.begin ();
+	for (; it != _positions.end () ; ++it) {
+		if (index < (*it)->_startIndex){
+			it = _positions.end ();
+			break;
+		}
+		else if (index <= (*it)->_startIndex + (*it)->_length)
+			break;
+	}
 
-   if (it == _positions.end ())
-      return _weight;
-   else {
-      return (*it)->_weight;
-   }
+	if (it == _positions.end ())
+		return _weight;
+	else {
+		return (*it)->_weight;
+	}
 }
 
 
@@ -48,90 +49,106 @@ double SeqWeightDB::PositionalWeight::getPositionWeight (int index) const
 // >YOL116W 0.991742546801741
 
 struct WeightsReader {
-   virtual ~WeightsReader () {
-   }
-   void read (const char* fname) {
-      
-      std::ifstream in (fname);
-      if (! in.is_open()) {
-         mmustfail (
-            FixedStrBuffer<4 * 1024> ("unable to open weight file %s",  fname)
-            );
-      }
-      read (in);
-   }
-   void read (std::istream& in) {
-      std::string name, s;
-      while (true) {
-         s.resize (0);
-         name.resize (0);
+	virtual ~WeightsReader () {
+	}
+	void read (const char* fname) {
 
-         bool ok = true;
-         while (ok && s.size()==0) {
-            std::istream::sentry _ok(in);
-            if (ok = _ok)
-               in>>s;// skip empty lines;
-         }
+		std::ifstream in (fname);
+		if (! in.is_open()) {
+			mmustfail (
+				FixedStrBuffer<4 * 1024> ("unable to open weight file %s",  fname)
+				);
+		}
+		read (in);
+	}
+	void read (std::istream& in) {
+		std::string name, s;
+		while (true) {
+			s.resize (0);
+			name.resize (0);
 
-         if (!ok)
-            break;    
+			bool ok = true;
+			while (ok && s.size()==0) {
+				std::istream::sentry _ok(in);
+				if (ok = _ok)
+					in>>s;// skip empty lines;
+			}
 
-         // here we handle cases in which the ">" is/isn't separated from the name of the gene.
-         if (s ==">")
-            in>>name;
-         else {
-            assert(s[0]=='>');    
-            name = s.substr(1);
-         }
+			if (!ok)
+				break;    
 
-         readValue(in, Str (name));
-      }
-   }
+			// here we handle cases in which the ">" is/isn't separated from the name of the gene.
+			if (s ==">")
+				in>>name;
+			else {
+				assert(s[0]=='>');    
+				name = s.substr(1);
+			}
 
-   virtual void readValue (std::istream& in, const Str&) = 0;
+			/// remove trailing whitespace
+			boost::trim (name);
+
+			/// check if the name is unique in the weight file
+			std::pair <NameSet::iterator, bool> result = _entryNames.insert (name);
+			if (!result.second) {
+				/// we have already seen this sequence name
+				throw BaseException (
+					StrBuffer (Str ("The sequence named \""), Str (name), Str ("\" appears more than once in the weight file"))
+				);
+			}
+
+			readValue(in, Str (name));
+		}
+	}
+
+	virtual void readValue (std::istream& in, const Str&) = 0;
+
+protected:
+	typedef std::set <std::string> NameSet;
+	NameSet _entryNames;
 };
 
 /*
 AutoPtr <SeqWeightDB::Name2Weight> 
-   SeqWeightDB::readWgtFile (const char* wgtFileName)
+SeqWeightDB::readWgtFile (const char* wgtFileName)
 {
-   //
-   // we are not expected to deal with more than 
-   struct SeqWeightsReader : public WeightsReader, AutoPtr <Name2Weight> {
-      SeqWeightsReader () { 
-         set (new Name2Weight (_default_db_size)); 
-      }
-      virtual void readValue (std::ifstream& in, 
-                              const Str& name) 
-      {
-         double val;
-         in>>val;
-         get ()->add(new SeqWeightDB::Name2Weight::Node(Str (name), val));
-      }
-   } reader;
+//
+// we are not expected to deal with more than 
+struct SeqWeightsReader : public WeightsReader, AutoPtr <Name2Weight> {
+SeqWeightsReader () { 
+set (new Name2Weight (_default_db_size)); 
+}
+virtual void readValue (std::ifstream& in, 
+const Str& name) 
+{
+double val;
+in>>val;
+get ()->add(new SeqWeightDB::Name2Weight::Node(Str (name), val));
+}
+} reader;
 
-   reader.read (wgtFileName);
-   return reader.release();
+reader.read (wgtFileName);
+return reader.release();
 }
 */
 
 AutoPtr <SeqWeightDB::ID2Weight> 
-   SeqWeightDB::computeWeightIndex (
-      const Name2Weight& weights, const SequenceDB& db)
+SeqWeightDB::computeWeightIndex (
+											const Name2Weight& weights, const SequenceDB& db)
 {
-   AutoPtr <ID2Weight> id2weight = new ID2Weight;
+	AutoPtr <ID2Weight> id2weight = new ID2Weight;
 
-   Name2Weight::const_iterator it = weights.begin ();
-   for (;it != weights.end () ; ++it) {
-      Sequence* seq = db.getSequence(it->first);
-      debug_mustbe (seq);
-      if (seq) {
-         PositionalWeight_var weight = it->second;
-         (*id2weight) [seq->id ()] = weight;
-      }
-   }
+	Name2Weight::const_iterator it = weights.begin ();
+	for (;it != weights.end () ; ++it) {
+		Sequence* seq = db.getSequence(it->first);
+		debug_mustbe (seq);
+		if (seq) {
+			PositionalWeight_var weight = it->second;
+			(*id2weight) [seq->id ()] = weight;
+		}
+	}
 
-   return id2weight;
+	return id2weight;
 }
 
 //
@@ -148,79 +165,80 @@ AutoPtr <SeqWeightDB::ID2Weight>
 // >YOL116W 0.5 =[0, 70]  0.67 = [560, 700]
 struct PosWeightsReader : public WeightsReader 
 {
-   PosWeightsReader ()
-   {
-      _posweights = new SeqWeightDB::Name2Weight;
-      _entries.reserve (10);
-   }
+	PosWeightsReader ()
+	{
+		_posweights = new SeqWeightDB::Name2Weight;
+		_entries.reserve (10);
+	}
 
-   virtual void readValue (std::istream& sin, const Str& name)
-   {
-      double seqWeight;
-      sin>>seqWeight;
+	virtual void readValue (std::istream& sin, const Str& name)
+	{
+		double seqWeight;
+		sin>>seqWeight;
 
 		std::string buffer;
 		std::getline (sin, buffer);
-      readValue (buffer, name, seqWeight);
-   }
+		readValue (buffer, name, seqWeight);
+	}
 
 	void readValue (const std::string& lineBuffer, const Str& name, double seqWeight) {
 		Str buffer = Str (lineBuffer).trimmedSubstring();
 
-      _entries.clear ();
+		_entries.clear ();
 		while (!buffer.empty ()) {
-         double w;
-         int i1, i2;
-         int count = sscanf (buffer.getChars (), "%lf = [%d, %d]", &w, &i1, &i2);
+			double w;
+			int i1, i2;
+			int count = sscanf (buffer.getChars (), "%lf = [%d, %d]", &w, &i1, &i2);
 			Str::Index next = buffer.indexOf(']');
 			if ((next == Str::badIndex) || (count != 3) || (!(i1 <= i2) && (w>=0) && (w<=1) && (i1 >= 0))){
 				throw BaseException (
 					StrBuffer (
-						Str ("bad positional-weight format in "), name, 
-						Str (":\nUnexpected "), buffer
+					Str ("bad positional-weight format in "), name, 
+					Str (":\nUnexpected "), buffer
 					)
-				);
+					);
 			}
-              
-         _entries.push_back(new SeqWeightDB::PositionalWeight::Entry (i1, i2-i1, seqWeight * w));
+
+			/// this is a new entry
+			_entries.push_back(new SeqWeightDB::PositionalWeight::Entry (i1, i2-i1, seqWeight * w));
 			buffer = buffer.substring (next + 1);
 			buffer = buffer.trimmedSubstring();
-      }
+		}
 
-      _temp = name;
-      SeqWeightDB::PositionalWeight_var posWeight (
-         new SeqWeightDB::PositionalWeight (seqWeight, _entries)
-      );
+		_temp = name;
+		SeqWeightDB::PositionalWeight_var posWeight (
+			new SeqWeightDB::PositionalWeight (seqWeight, _entries)
+			);
 
-      _posweights->insert (
-         SeqWeightDB::Name2Weight::value_type (_temp, posWeight)
-      );
-   }
+		_posweights->insert (
+			SeqWeightDB::Name2Weight::value_type (_temp, posWeight)
+			);
+	}
 
-   virtual void readValue (Persistance::TextReader& reader, const Str& name, double seqWeight)
-   {
-      _temp.setLength (0);
-      reader.readln (_temp);
-   }
+	virtual void readValue (Persistance::TextReader& reader, const Str& name, double seqWeight)
+	{
+		_temp.setLength (0);
+		reader.readln (_temp);
+	}
 
-   StrBuffer _temp;
-   AutoPtr <SeqWeightDB::Name2Weight> _posweights;
-   SeqWeightDB::PositionalWeight::Positions _entries;
+	StrBuffer _temp;
+	AutoPtr <SeqWeightDB::Name2Weight> _posweights;
+	SeqWeightDB::PositionalWeight::Positions _entries;
 };
 
 
 AutoPtr <SeqWeightDB::Name2Weight> 
 SeqWeightDB::readWgtFile (const char* weightFileName)
 {
-   PosWeightsReader reader;
-   reader.read (weightFileName);
-   return reader._posweights;
+	PosWeightsReader reader;
+	reader.read (weightFileName);
+	return reader._posweights;
 }
 
 AutoPtr <SeqWeightDB::Name2Weight> 
 SeqWeightDB::readWgtFile (std::istream& in)
 {
-   PosWeightsReader reader;
-   reader.read (in);
-   return reader._posweights;
+	PosWeightsReader reader;
+	reader.read (in);
+	return reader._posweights;
 }
